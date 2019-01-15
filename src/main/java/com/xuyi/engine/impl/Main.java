1af10b2580ce633a5eab374a2cf7ff0202fe67c8
xuyi <yi.xu@ymm56.com>
2019-01-15 09:59:32 +0800

    代码优化

--- src/main/java/com/xuyi/engine/impl/Main.java
+++ src/main/java/com/xuyi/engine/impl/Main.java
@@ -3,7 +3,6 @@ package com.xuyi.engine.impl;
 import com.google.common.collect.Maps;
 
 import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
 
 public class Main {
     public static void main(String[] args) {
