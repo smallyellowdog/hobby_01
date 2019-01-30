34f9d174e3acec7c25157009c22539b8aeaa2b1d
xuyi <yi.xu@ymm56.com>
2019-01-30 14:31:20 +0800

    代码优化

--- src/main/java/com/xuyi/designpattern/simple/SingleDCLObject.java
+++ src/main/java/com/xuyi/designpattern/simple/SingleDCLObject.java
@@ -2,6 +2,7 @@ package com.xuyi.designpattern.simple;
 
 /**
  * 这种方式采用双锁机制，安全且在多线程情况下能保持高性能。
+ * volatile：保证指令不可重排序
  */
 public class SingleDCLObject {
     private volatile static SingleDCLObject singleDCLObject;
